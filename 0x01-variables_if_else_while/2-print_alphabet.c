607ade962ed0aa6bf618229d0e569cfba50f52fd
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-10 00:52:30 +0100

    I sometimes play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-prints out lowercase alphabets
+ *
+ * Return: Always 0 {Success)
+ */
+int main(void)
+{
+	/*Using for loop to print lowercase Alphabet */
+	char Alphabet;
+
+
+	for (Alphabet = 'a'; Alphabet <= 'z'; Alphabet++)
+	{
+		putchar(Alphabet);
+		putchar('\n');
+	return (0);
+	}
+}
