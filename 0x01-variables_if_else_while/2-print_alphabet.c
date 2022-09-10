b097e0d12e0b3030086b6382a94e987df3a881dd
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-10 03:17:47 +0100

    I sometimes play with alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main-prints out lowercase alphabets
+ * main-prints out lowercase alphabet
  *
- * Return: Always 0 {Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
-	/*Using for loop to print lowercase Alphabet*/
-	char letter;
+{                                                         char ch;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
-
-	putchar('\n');
-
-	return (0);
+        for (ch = 'a'; ch >= 'z'; ch++)
+                putchar(ch);
+        putchar('\n');
+                                                          return (0);
 }
