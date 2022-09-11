d0653ab9dede812398d453d8fcc3672df5faf874
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-11 12:39:45 +0000

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int dgt1, dgt2;
 
-	for (dgt1 = 0; dgt1 < 9; dgt1++)
+	for (dgt1 = 0; dgt1 < 5; dgt1++)
 	{
-		for (dgt2 = 0; dgt2 < 9; dgt2++)
+		for (dgt2 = 0; dgt2 < 6; dgt2++)
 		{
 
 			putchar((dgt1 % 10) + '0');
