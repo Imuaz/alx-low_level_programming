340c6682f666c8b63d936cd0257e984aa95be2b0
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-14 06:44:01 +0000

    Absolute value of an interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs -prints out the comuted value af an inte
+ * rger.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
