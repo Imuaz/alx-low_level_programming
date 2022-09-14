800e82a4075d161e95a69b1afce5e03cc6ce8a42
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-14 15:21:28 +0100

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if interger is positive or negative
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negstive(i);
+
+	return (0);
+}
