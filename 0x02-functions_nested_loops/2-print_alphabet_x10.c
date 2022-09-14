7df1acf4c5d1d118e8ac1daaf272672ae8e25425
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-14 02:25:29 +0000

    Lowercase ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10-prints out lowercase alphab
+ * et ten times (x10)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char Alphabet;
+
+	while (i++ <= 9)
+	{
+		for (Alphabet = 'a'; Alphabet <= 'z';)
+			_putchar(Alphabet);
+		Alphabet++;
+		_putchar('\n');
+	}
+}
