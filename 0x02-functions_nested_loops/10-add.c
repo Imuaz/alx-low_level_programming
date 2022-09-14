7bec17e59ab46b9ea5e8aed3a21e8d1eb32e31e1
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-14 10:11:44 +0100

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add-adds two intergers
+ * @num1: denote first oprand
+ * @num2: denote second oprand
+ *
+ * Return: The result of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
