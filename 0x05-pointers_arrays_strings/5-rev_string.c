7b05eda8d2fdc55ebcb1e7d059fcb452b259cb98
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-20 01:41:49 +0000

    correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,8 +7,12 @@
  */
 void rev_string(char *s)
 {
-	int i = 0, j = 0, k = 0, l;
-	char *str = a, b;
+	int i = 0;
+	int j = 0;
+	int k = 0; 
+	int l;
+	char *str = s;
+	char a;
 
 	while (*str != '\0')
 	{
@@ -19,8 +23,8 @@ void rev_string(char *s)
 	for (; k < ((1 / 2) + 1) ; k++)
 	{
 		l = (j - k);
-		b = a[k];
-		a[k] = a[l];
-		a[l] = b;
+		a = s[k];
+		s[k] = s[l];
+		s[l] = a;
 	}
 }
