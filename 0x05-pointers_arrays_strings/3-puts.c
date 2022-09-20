278b2678fd89acca516ffccc52fcdf5671e77953
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-20 01:30:58 +0000

    testcorrection

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,12 +7,12 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int l = 0;
 
-	while (*(str + 1) != '\0')
+	while (*(str + l) != '\0')
 	{
-		_putchar(str[1]);
-		i++;
+		_putchar(str[l]);
+		l++;
 	}
 	_putchar('\n');
 }
