8b861359366baf9d6c7d84a6858cb300e4bfe021
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-20 11:37:52 +0100

    test

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - converts a string to an inter
+ * @s: the string to be coverted
+ * Return: result*sign
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int i = 0;
+	int sign = 1;
+
+	if (*s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (*s[i])
+	{
+		if (!(*s[i] - '0' >= 0 && *s[i] - '0' <= 9))
+		return (-1);
+
+		result = result * 10 + *s[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
