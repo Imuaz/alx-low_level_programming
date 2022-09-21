bb46f6844e1d15eec76092aadfbb047c89ab0ff8
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-21 22:50:47 +0000

    Task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * *cap_string-capitalizes all words of a str
+ * ing.
+ * @str: the string to be capitalized
+ *
+ * Return: resultin str
+ */
+char *cap_string(char *str)
+{
+	int i;
+	
+	i = 0;
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] = str[0] - 32;
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		switch (str[i])
+		{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '\n':
+			case '\t':
+				if (str[i + 1] > 96 && str[i + 1] < 123)
+		{
+					str[i + 1] = str[i + 1] - 32;
+				}
+		}
+	}
+	return (str);
+}
