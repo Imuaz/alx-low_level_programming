e94b2772d9ac3727a7c6dc31e095230d72e42e3b
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-21 22:57:06 +0000

    Task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,35 +9,31 @@
  */
 char *cap_string(char *str)
 {
-	int i;
-	
-	i = 0;
-	if (str[0] >= 'a' && str[0] <= 'z')
+	int i = 0;
+
+	while (str[i])
 	{
-		str[0] = str[0] - 32;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0)
+			str[i] -= 32;
+
+		i++;
 	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		switch (str[i])
-		{
-			case ',':
-			case ';':
-			case '.':
-			case '!':
-			case '?':
-			case '"':
-			case '(':
-			case ')':
-			case '{':
-			case '}':
-			case ' ':
-			case '\n':
-			case '\t':
-				if (str[i + 1] > 96 && str[i + 1] < 123)
-		{
-					str[i + 1] = str[i + 1] - 32;
-				}
-		}
-	}
+
 	return (str);
 }
