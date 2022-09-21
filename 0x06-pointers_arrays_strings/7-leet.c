ecc2e2827ef0312c94ff0431f957091413b8c9dd
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-21 23:12:51 +0000

    Task6

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string in to see
+ * @str:the string to be encoded
+ * Return: a pointer to the reaultant auto
+ */
+char *leet(char *str)
+{
+	int i = 0, j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	while (str[i])
+	{
+		for (j = 0; j <= 7; j++)
+	{
+		if (str[i] == leet[j] ||
+				str[i] - 32 == leet[j])
+				str[i] = j + '0';
+	}
+
+		i++;
+	}
+
+	return (str);
+}
