157f67bb3a4aef13e7f8f19cbdcef333e9f266ab
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-21 19:24:04 +0000

    Task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,32 @@
 #include "main.h"
 
 /**
- * *_strncat - concatenates two strings just like strcat but uses n-bytes from
- * src and src does not need null terminating character '\0'
+ * *_strncat - concatenates two strings just
+ * ike strcat but uses n-bytes from
+ * src and src does not need null terminating
+ * character '\0'
  * @dest: the destination string
- * @scr: the source string
- * Return:a pointer to the resulting dest string
+ * @src: the source string
+ * @n: nth character of the source string
+ * Return:a pointer to the resulting dest str
+ * ng
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, j;
 
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
