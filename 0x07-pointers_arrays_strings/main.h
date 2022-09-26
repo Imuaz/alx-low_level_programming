f4554b73ce0b2089721cf4f0d8e0f4e6db21de04
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-26 21:52:38 +0000

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,8 @@
  * ototype functions of th 0x07-even_more_po
  * inters arrays and strings
  */
+#include <string.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
