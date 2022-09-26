071f6e67a68b7d8a6b1aca72ee2390001f07e605
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-26 23:24:10 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix
+ * substring.
+ * @s:the string to search from
+ * @accept:the prefix substring to measure
+ * Return: returns the number of bytes in
+ * the initial segment of s which consist
+ * only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len;
+
+	len = strspn(accept, s);
+
+	return (len);
+}
