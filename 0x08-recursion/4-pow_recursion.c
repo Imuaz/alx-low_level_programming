cc12d248c07dc8428a016548dad498340845f465
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-09-29 01:10:45 +0000

    Recursion correction

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,9 +12,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (n == 0)
+	if (y == 0)
 	{
-		return (1); /*since any number**0 is 1*/
+		return (1);
 	}
 	return (x * _pow_recursion(x, y - 1));
 }
