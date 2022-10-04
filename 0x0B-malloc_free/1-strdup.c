d7bcb89ce07682adf92b4d4b7a4e57d9f2440478
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-04 23:11:48 +0000

    finally Dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 /**
  * *_strdup - returns a pointer to a newly allocated space in
  * memory, which contains a copy of the string given as a parameter.
- * @*str: string to be coppied
+ * @str: string to be coppied
  * Return: returns a pointer to the duplicated string on success
  * and NULL is str=NULL or insufficient memory was available
  */
