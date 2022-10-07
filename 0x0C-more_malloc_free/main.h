ff38dbf8d7c0291f9b8a254a293126cfc95db5d2
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-07 00:22:44 +0000

    corected

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -16,5 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int main(int argc, char const *argv[]);
+void print_int(unsigned long int n);
+void _puts(char *str);
+
 #endif /*MAIN_H*/
