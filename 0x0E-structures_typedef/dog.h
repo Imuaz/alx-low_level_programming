a18a3260c238422600a9f5ff7d555c8f9e131ca5
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-10 18:16:42 +0100

    tasks 0&1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,7 +24,7 @@ struct dog
 /**
  * dog_t - A typedef for struct dog
  */
-typedef struct dog dog_t;
+//typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
