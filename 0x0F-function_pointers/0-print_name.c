1f649b849eff84ce04c3d9f930dc48624eb36b5e
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-12 23:05:51 +0100

    0-2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,11 @@
 /**
  * print_name - prints a name
  * @name: pointer to char
- * @f: function pointer that returns nothing
+ * @f: pointer to function that returns nothing
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && *name != '\0' && f != NULL)
-		_putchar(name);
+		f(name);
 }
