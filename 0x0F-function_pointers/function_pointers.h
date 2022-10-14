40b4f5471178e1691d41fca9f8dcfdd7150ad2e7
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-14 22:20:53 +0100

    complete

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,14 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
 #include <stdio.h>
 #include <stdlib.h>
-/**
+
+/*
  * file: function_pointers.h
  * Auth: Idris Muazu Yakub
  * Desc: Header file containing all the function prototypes
- * used in the 0x0F. C-funtion pointers project
+ *       used in the 0x0F. C-funtion pointers project
  */
 
 int _putchar(char c);
