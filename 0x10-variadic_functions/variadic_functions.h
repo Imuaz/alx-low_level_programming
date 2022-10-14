ea35d4bd5bfe020739da8ee1707df73f57b2b199
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-14 22:08:49 +0100

    complete

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -24,6 +24,11 @@ typedef struct printer
 
 } printer_t;
 
+void print_i(va_list list, char *s);
+void print_c(va_list list, char *sep);
+void print_s(va_list list, char *sep);
+void print_f(va_list list, char *sep);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
