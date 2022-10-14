b62d4ccf8444b606def1a8b95954bef9321f00c2
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-14 14:55:48 +0100

    requarment

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,16 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list prm;
-	unsigned int i;
-	int sum;
+	unsigned int i, sum;
 
 	if (n == 0)
 		return (0);
 	sum = 0;
 	va_list(prm, n);
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(prm, int);
-	}
+
 	va_end(prm);
+
 	return (sum);
 }
