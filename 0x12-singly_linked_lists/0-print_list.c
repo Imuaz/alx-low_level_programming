8b32c0d2fc73ae4bea81b686ae977a3619505e1b
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-20 22:26:20 +0000

    complete before review

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t nodes = 0;
 
-	if (h->str == NULL)
+	while (h)
 	{
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-	}
-	for (i = 0; h; i++)
-	{
-		printf("[%d] %s\n", h->len, h->str);
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
 	}
-	return (i);
+
+	return (nodes);
 }
