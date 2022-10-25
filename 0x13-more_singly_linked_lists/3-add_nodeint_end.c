018946ebd97d46f62950cbb6a4aa735279795121
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-25 23:31:31 +0000

    Task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,9 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	current = *head;
 	while (current->next != NULL)
-	{
 		current = current->next;
 	current->next = new;
-	}
 	return (*head);
 }
