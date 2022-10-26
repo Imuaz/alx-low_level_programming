258195e804d79f159846ecdf5db28ddc2189c96c
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-10-26 14:58:53 +0100

    test102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,45 +1,93 @@
-#include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
+
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
 
 /**
-* free_listint_safe - A function that frees a list
-* @h: A pointer listint_t structure
-* Return: The size of the list that was free'd
-*/
-size_t free_listint_safe(listint_t **h)
+ * looped_listint_count - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return:the number of unique nodes in the list or 0 if the list is not looped
+ */
+size_t looped_listint_count(listint_t *head)
 {
-	size_t counter = 0;
-	listint_t *temp;
+	listint_t *current, *new;
+	size_t nodes = 1;
 
-	temp = *h;
-	while (temp)
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	current = head->next;
+	new = (head->next)->next;
+
+	while (new)
 	{
-		temp = *h;
-		temp = temp->next;
-		free_list(temp);
-		counter++;
-	}
-	*h = NULL;
+		if (current == new)
+		{
+			current = head;
+			while (current != new)
+			{
+				nodes++;
+				current = current->next;
+				new = new->next;
+			}
 
-	return (counter);
+			current = current->next;
+			while (current != new)
+			{
+				nodes++;
+				current = current->next;
+			}
+
+			return (nodes);
+		}
+
+		current = current->next;
+		new = (new->next)->next;
+	}
+
+	return (0);
 }
 
 /**
-* free_list - A function that frees a listint_t recursively
-* @head: A pointer to the listint_t structure
-* Return: Nothing
-*/
-void free_list(listint_t *head)
+ * free_listint_safe - frees a listint_t list safely.
+ * @h: a pointer to address of the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
+ */
+size_t free_listint_safe(listint_t **h)
 {
-	listint_t *temp;
+	listint_t *tmp;
+	size_t nodes, idx;
 
-	if (head)
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
 	{
-		temp = head;
-		temp = temp->next;
-		free(temp);
-		free_list(temp);
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
 	}
-	free(head);
+
+	else
+	{
+		for (idx = 0; idx < nodes; idx++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
 }
