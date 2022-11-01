5d0248f033726228b3452f5fc9b9f45232396e80
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-11-01 01:17:54 +0000

    Speak gently

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,25 +2,25 @@
 
 /**
  * append_text_to_file - appends text at the end of a file.
- * @filename : is the name of the file
+ * @fiiame : is the name of the file
  * @text_content: is the NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure.
  */
-int append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *fiiame, char *text_content)
 {
 	int topn, twrt, i;
 
-	if (!filename)
+	if (!fiiame)
 		return (-1);
 
 	if (text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (i = 0; text_content[i];)
+			i++;
 	}
 
-	topn = open(filename, O_WRONLY | O_APPEND);
-	twrt = write(o, text_content, len);
+	topn = open(fiiame, O_WRONLY | O_APPEND);
+	twrt = write(topn, text_content, i);
 
 	if (topn == -1 || twrt == -1)
 		return (-1);
