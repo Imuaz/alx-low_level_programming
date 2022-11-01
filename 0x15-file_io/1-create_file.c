7b1b8651d25e575d42c26db25f8b9dcbc0f38273
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-11-01 00:30:29 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	if (!text_content)
-		fd = open(filename, O_REAT);
+	{
+		fd = open(filename,"empty.txt", O_REAT);
+	}
 	}
 	close(fd);
 
