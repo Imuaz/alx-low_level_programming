a846d8426ba06687a0c2c97160c84aab5dc0b888
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-11-01 00:09:26 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int *fd, i;
+	int fd, i;
 	ssize_t cfwrite;
 
 	if (!filename)
@@ -18,18 +18,16 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i];)
 			i++;
-	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	cfwrite = write(fd, text_content, i);
-
 	if (fd == -1)
 		return (-1);
-
+	cfwrite = write(fd, text_content, i);
 	if (cfwrite == -1)
 	{
 		write(1, "fails", 6);
 		return (-1);
+	}
 	}
 	close(fd);
 
