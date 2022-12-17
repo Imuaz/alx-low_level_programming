4df3738cb65aa132b6038f9aaf522ae5b716321c
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-12-17 21:14:19 +0000

    Readme review and files recovery

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 99; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
 		num2 = num1 + 1;
-		while (num2 < 99)
+		while (num2 <= 99)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
@@ -20,7 +20,7 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 			num2++;
-			if (num1 <= 98)
+			if (num1 < 98)
 			{
 				putchar(',');
 				putchar(' ');
