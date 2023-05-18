c00c5cb4a80d54da29f8a5d4ebf13718349fea87
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-18 11:47:06 +0000

    readme reveiw and correction

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -39,7 +39,7 @@ int interpolation_search(int *array, size_t size, int value)
 			high = pos - 1;
 		}
 	}
-	printf("Value checked array[%ld] is out of range\n", high);
+	printf("Value checked array[%lu] is out of range\n", high);
 
 	return (-1);/*Value not found*/
 }
