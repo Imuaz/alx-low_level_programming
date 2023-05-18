4787759d9063823315a193a71dc4c053d54ecd87
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-18 13:21:56 +0000

    readme reveiw and correction

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -24,7 +24,7 @@ int interpolation_search(int *array, size_t size, int value)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 		if (pos < size)
-			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		else
 		{
 			printf("Value checked array[%lu] is out of range\n", pos);
