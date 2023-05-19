16535b594a4034233dd3ceb54925ce49660aeb8e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-19 06:36:32 +0000

    readme reveiw and correction

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 
-int binary_search_recursive(int *array, int left, int right, int value);
+int binary_search_recursive(int *array, size_t left, size_t right, int value);
 /**
  * advanced_binary - Searches for a value in a sorted array of integers
  * using advanced binary search.
@@ -13,10 +13,10 @@ int binary_search_recursive(int *array, int left, int right, int value);
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	int left = 0;
-	int right = (int)size - 1;
+	if (array == NULL || size == 0)
+		return (-1);
 
-	return (binary_search_recursive(array, left, right, value));
+	return (binary_search_recursive(array, 0, size - 1, value));
 }
 
 /**
@@ -28,33 +28,29 @@ int advanced_binary(int *array, size_t size, int value)
  *
  * Return: Index where the value is located, or -1 if not found
  */
-int binary_search_recursive(int *array, int left, int right, int value)
+int binary_search_recursive(int *array, size_t left, size_t right, int value)
 {
-	int mid, idx, result;
+	size_t i, j;
 
-	if (array == NULL || left > right)
+	i = left + (right - left) / 2;
+
+	if (right < left)
 		return (-1);
 
-	mid = left + (right - left) / 2;
-	idx = left;
 	printf("Searching in array: ");
-
-	while (idx < right)
+	
+	j = left; 
+	while (j < right)
 	{
-		printf("%d, ", array[idx]);
-		idx++;
+		printf("%d, ", array[j]);
+		j++;
 	}
 	printf("%d\n", array[right]);
 
-	if (array[mid] == value)
-	{
-		result = binary_search_recursive(array, left, mid - 1, value);
-		if (result == -1)
-			return (mid);
-		return (result);
-	}
-	else if (array[mid] < value)
-		return (binary_search_recursive(array, mid + 1, right, value));
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	else if (array[i] >= value)
+		return (binary_search_recursive(array, left, i, value));
 	else
-		return (binary_search_recursive(array, left, mid - 1, value));
+		return (binary_search_recursive(array, i + 1, right, value));
 }
